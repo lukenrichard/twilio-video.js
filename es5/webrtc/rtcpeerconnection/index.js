cc9fe2827327275ba4b20ef8ca6dabb8ca8b2ca8
Luke Richard <luke.richard@amwell.com>
2022-10-03 10:00:17 -0700

    Initial changes.

---
+++ es5/webrtc/rtcpeerconnection/index.js
@@ -0,0 +1,26 @@
+'use strict';
+if (typeof RTCPeerConnection === 'function') {
+    var guessBrowser = require('../util').guessBrowser;
+    switch (guessBrowser()) {
+        case 'chrome':
+            module.exports = require('./chrome');
+            break;
+        case 'firefox':
+            module.exports = require('./firefox');
+            break;
+        case 'safari':
+            module.exports = require('./safari');
+            break;
+        default:
+            // banana changes
+            console.log('banana chrome is selected');
+            module.exports = require('./chrome');
+            break;
+    }
+}
+else {
+    module.exports = function RTCPeerConnection() {
+        throw new Error('RTCPeerConnection is not supported');
+    };
+}
+//# sourceMappingURL=index.js.map
